547107e2cc36d7fc8f78d5533ec6303314337db9
AlanDev <alansoftware2003@micorreo.upp.edu.mx>
2024-07-30 18:41:40 -0600

    Optimization of Sum function

--- func.py
+++ func.py
@@ -1,9 +1,4 @@
-def suma (a, b, c=0, d=0):
-    if c != 0 and d != 0:
-        return a + b + c + d
-    elif c != 0:
-        return a + b + c
-    else:
-        return a + b
+def suma (*num):
+    return sum(num)
     
-print(suma(2, 5, 3, 4))
+print(suma(2, 5, 3, 4, 5))
